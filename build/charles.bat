5b4f6bb26d66fc0e6e83cf0ff5a92ae3e9f0f329
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-04 10:41:03 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -1,9 +1,8 @@
 @echo off
 echo Processo de testes automatizados Charles
-pause
+
 if exist "config.xml" (
-    rem "Executando o Charles Console usando o arquivo config.xml"
+    echo "Executando o Charles Console usando o arquivo config.xml"
 ) else (
-    rem "Arquivo de configuração não encontrado"
+    echo "Arquivo de configuração não encontrado"
 )
-pause

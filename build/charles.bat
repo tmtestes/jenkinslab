2248c5845e57bf52745d789a9bb53606251c3bc1
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-04 17:38:25 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -3,7 +3,7 @@ echo Processo de testes automatizados Charles
 
 if exist c:/Charles/config.xml (
     echo Executando o Charles Console usando o arquivo config.xml
-    start /min cmd /c "c:/Charles/Executavel/CharlesConsole.exe c:/Charles/config.xml || exit %^errorLevel%"
+    start /min cmd /c "c:\Charles\Executavel\CharlesConsole.exe c:\Charles\config.xml || exit %^errorLevel%"
 ) else (
     echo Arquivo de configuração não encontrado
     exit 1

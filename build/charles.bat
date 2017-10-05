e6342801f0bfcd8c10c227114618ada2e11334d5
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-05 16:54:48 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -3,7 +3,9 @@ echo Processo de testes automatizados Charles
 
 if exist c:/Charles/config.xml (
     echo Executando o Charles Console usando o arquivo config.xml
-    c:\Charles\Executavel\CharlesConsole.exe "c:\Charles\config.xml"|rem
+    REM c:\Charles\Executavel\CharlesConsole.exe "c:\Charles\config.xml"|rem
+    C:\poc\headlesschrome\selenium_with_nodes.exe|rem 
+    exit 0   
 ) else (
     echo Arquivo de configuração não encontrado
     exit 1

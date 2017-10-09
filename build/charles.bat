1be1de846f64c43fc2ab492b4ff0b4cc9dcf953c
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-09 14:02:31 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -7,7 +7,7 @@ if exist c:/Charles/config.xml (
     start /B /WAIT "" "c:\Charles\Release\CharlesConsole.exe" "c:\Charles\config.xml"
     REM start /B /WAIT "" "C:\poc\headlesschrome\selenium_with_nodes.exe" 
     echo Fim a execução do Charles Console usando o arquivo config.xml
-
+    emailext attachLog: true, body: 'Teste finalizado com sucesso', subject: 'Pipeline, to: 'mauricio.freire@tmtestes.com.br'
     exit 0   
 ) else (
     echo Arquivo de configuração não encontrado

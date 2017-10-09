f09e558f27e0fa0877d4e8be6164b584224163b2
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-09 11:03:41 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -4,8 +4,8 @@ echo Processo de testes automatizados Charles
 if exist c:/Charles/config.xml (
     echo Iniciando a execução do Charles Console usando o arquivo config.xml
     echo Detalhes:  
-    REM c:\Charles\Release\CharlesConsole.exe "c:\Charles\config.xml"|rem
-    start /B /WAIT "" "C:\poc\headlesschrome\selenium_with_nodes.exe" 
+    start /B /WAIT "" "c:\Charles\Release\CharlesConsole.exe" "c:\Charles\config.xml"
+    REM start /B /WAIT "" "C:\poc\headlesschrome\selenium_with_nodes.exe" 
     echo Fim a execução do Charles Console usando o arquivo config.xml
 
     exit 0   

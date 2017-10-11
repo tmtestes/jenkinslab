25a5f529090ed43ef2b0f1dd577cf1f610d8717f
tmtestes <31699229+tmtestes@users.noreply.github.com>
2017-10-11 16:34:40 -0300

    Update charles.bat

--- build/charles.bat
+++ build/charles.bat
@@ -1,7 +1,8 @@
 
 CLS
 @ECHO OFF
-START /B /WAIT  c:\poc\debug\selenium_with_nodes.exe
+REM START /B /WAIT  c:\poc\debug\selenium_with_nodes.exe
+START /B /WAIT  C:\Charles\Release\CharlesConsole.exe "C:\Charles\config.xml"
 SET RET=%errorlevel%
 ECHO Return: %RET%
 EXIT %RET%
